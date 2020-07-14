a4f5b1a23450f51b9d6557e5a476db76eedba03c
Armando Coria <48301256+corria9@users.noreply.github.com>
2020-07-13 21:37:06 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
+var base;
+var playPause = 0;
 
+function playFunc () {
+	play ();
+	playPause = playPause + 1;
+
+	if (playPause === 1){
+		document.getElementById("play").classList.add("pause");
+	} else if (playPause === 2) {
+		document.getElementById("play").classList.remove("pause");
+		playPause = 0;
+		stop();
+	}
+}
+
+function play () {
+	
+}
+
+function stop () {
+	
+}
+
+var millisecond = 0;
+var second = 0;
